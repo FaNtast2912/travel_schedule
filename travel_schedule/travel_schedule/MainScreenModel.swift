3ea703dc0d4062175f170c3d0d2803651b35adef
FaNtast2912 <z_maks@inbox.ru>
2025-02-12 18:15:50 +0300

    Вынес коллектор ошибок в отдельный файл

--- travel_schedule/travel_schedule/MainScreenModel.swift
+++ travel_schedule/travel_schedule/MainScreenModel.swift
@@ -7,18 +7,6 @@
 import Foundation
 import OpenAPIURLSession
 
-private actor ErrorCollector {
-    private(set) var errors: [Error] = []
-    
-    func addError(_ error: Error) {
-        errors.append(error)
-    }
-    
-    func getAllErrors() -> [Error] {
-        return errors
-    }
-}
-
 class MainScreenModel: ObservableObject {
     
     private let service = TravelServiceFacade.shared
