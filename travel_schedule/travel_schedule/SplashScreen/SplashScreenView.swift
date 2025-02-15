31175affcca11be3312f33fe8975c50c27729dfd
FaNtast2912 <z_maks@inbox.ru>
2025-02-15 19:42:35 +0300

    добавил сплешскрин

---
+++ travel_schedule/travel_schedule/SplashScreen/SplashScreenView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  travel_schedule
+//
+//  Created by Maksim Zakharov on 15.02.2025.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    
+    var body: some View {
+        Group {
+            if isActive {
+                MainScreen()
+            } else {
+                SplashView()
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                withAnimation {
+                    isActive = true
+                }
+            }
+        }
+    }
+}
+
+#if DEBUG
+#Preview {
+    SplashScreenView()
+}
+#endif
